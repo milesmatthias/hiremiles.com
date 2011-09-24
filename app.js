6b6677b6a669be3bfa788fcab1ab5eb148549b5c
Miles <miles.matthias@gmail.com>
2011-09-24 16:41:56 -0500

    Changed port to something else - according to Heroku

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(80);
+app.listen(25982);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
